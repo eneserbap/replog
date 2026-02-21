54640926a6cd4743fa1439b6fa5a5fac74006c93
alpemreelmas <elmasalpemre@gmail.com>
2026-02-21 09:04:09 +0100

    refactor: Migrate project to TypeScript, update ESLint and Vite configurations, and remove old JavaScript/JSX application files.

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
@@ -7,6 +8,7 @@ import path from "path";
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
